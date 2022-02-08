747e5dca6e6a184276c34810a16ab87dddc5f45f
Akanbi Dire <direakanbi@icloud.com>
2022-02-08 12:25:48 +0100

    Created website header and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Missions from './components/Missions';
+import Rockets from './components/Rockets';
+import MyProfile from './components/MyProfile';
+
 const App = () => (
-  <div className="App" />
+  <div className="app">
+    <Header />
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <Rockets />)}
+      />
+      <Route
+        path="/missions"
+        element={(
+          <Missions />)}
+      />
+      <Route
+        path="/profile"
+        element={(
+          <MyProfile />)}
+      />
+    </Routes>
+  </div>
 );
 
 export default App;
---
+++ src/components/Missions.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Missions = () => <div>Draft</div>;
+
+export default Missions;
