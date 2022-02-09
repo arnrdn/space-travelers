f88d8501828b94acd218794ec1c47230b227fa63
Akanbi Dire <direakanbi@icloud.com>
2022-02-09 16:19:05 +0100

    Created tests for React Components

---
+++ src/components/rockets/RocketProfile.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const RocketProfile = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  return (
+    <div className="reserve-container-top">
+      <h2>My Rockets</h2>
+      <ul className="reserve-container">
+        {reservedRockets.length === 0 && <p>No Reserved Rockets</p>}
+        {reservedRockets.map((rocket) => (
+          <li key={rocket.id} className="reserve-item">{rocket.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RocketProfile;
